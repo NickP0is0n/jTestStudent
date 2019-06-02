3939fb4d3d8daf8af43dba74be063988341e6ca8
NickP0is0n <nickchaywork@gmail.com>
2019-06-02 13:44:38 +0300

    Changed version number

---
+++ src/main/java/me/NickP0is0n/Main.java
@@ -0,0 +1,49 @@
+package me.NickP0is0n;
+
+import javafx.application.Application;
+import javafx.embed.swing.SwingFXUtils;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+import jfxtras.styles.jmetro8.JMetro;
+
+import java.io.IOException;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("jTest.fxml"));
+        new JMetro(JMetro.Style.LIGHT).applyTheme(root);
+        primaryStage.getIcons().add(new Image(getClass().getClassLoader().getResourceAsStream("logo.png")));
+        primaryStage.setTitle("jTest Student");
+        primaryStage.setScene(new Scene(root, 640, 400));
+        primaryStage.setResizable(false);
+        java.awt.Image logo = SwingFXUtils.fromFXImage(new Image(Main.class.getClassLoader().getResourceAsStream("logo.png")), null);
+        if(System.getProperty("os.name").equals("Mac OS X")) com.apple.eawt.Application.getApplication().setDockIconImage(logo); //для иконки в доке macOS
+        primaryStage.show();
+    }
+
+    public static FXMLLoader makeLoader (String fxmlFile) {
+        FXMLLoader loader = new FXMLLoader(Main.class.getClassLoader().getResource(fxmlFile));
+        return loader;
+    }
+
+    public static Stage startStage (FXMLLoader loader, String title, int width, int height, boolean resizable) throws IOException {
+        Stage stage = new Stage();
+        stage.getIcons().add(new Image("file:/resources/logo.png"));
+        stage.setTitle(title);
+        Parent root = (Parent)loader.load();
+        new JMetro(JMetro.Style.LIGHT).applyTheme(root);
+        Scene scene = new Scene(root, width, height);
+        stage.setScene(scene);
+        stage.setResizable(resizable);
+        return stage;
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
---
+++ src/main/java/me/NickP0is0n/TaskSwitchController.java
@@ -0,0 +1,112 @@
+package me.NickP0is0n;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.util.Date;
+
+public class TaskSwitchController{
+
+    private TaskSet currentTaskSet;
+    public boolean[] isTaskFinished;
+    private Stage currentStage;
+
+    @FXML
+    private ChoiceBox<String> taskSelector;
+
+    @FXML
+    private Button startTaskBtn;
+
+    void setCurrentTaskSet(TaskSet currentTaskSet, Stage stage) {
+        this.currentTaskSet = currentTaskSet;
+        isTaskFinished = new boolean[currentTaskSet.size()];
+        for (boolean i:
+             isTaskFinished) {
+                i = false;
+        }
+        updateSelector(0);
+        this.currentStage = stage;
+    }
+
+    @FXML
+    void endWork(ActionEvent event) {
+        for (int i = 0; i < currentTaskSet.size(); i++)
+        {
+            if (!isTaskFinished[i]) Controller.currentStudent.addTask(i, 0);
+        }
+        Controller.currentStudent.setFinishTime(new Date());
+        FileChooser chooser = new FileChooser(); //діалог збереження
+        chooser.setTitle("Save the result file");
+        chooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("jTest result files (.jres)", "*.jres")); //фильтр файлов
+        File taskFile = chooser.showSaveDialog(new Stage()); //показ диалога на отдельной сцене
+        if (taskFile != null) {
+            try {
+                taskFile.createNewFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+                showAlert("An error occurred while creating a file!", Alert.AlertType.ERROR);
+            }
+        }
+        if (taskFile != null) {
+            try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(taskFile))) {
+                oos.writeObject(Controller.currentStudent);
+            } catch (Exception e) {
+                e.printStackTrace();
+                showAlert("An error occurred while saving a file!", Alert.AlertType.ERROR);
+            }
+            showAlert("Your work has been successfully completed and saved.", Alert.AlertType.INFORMATION);
+            currentStage.close();
+        }
+    }
+
+    @FXML
+    void startTask(ActionEvent event) throws IOException {
+        if (isTaskFinished[getSelectedItemIndex(taskSelector.getValue())]) showAlert("You have already tried to perform this task!", Alert.AlertType.ERROR);
+        else
+        {
+            FXMLLoader loader = Main.makeLoader("taskSolving.fxml");
+            Stage stage = Main.startStage(loader, "Task solving", 600, 634, false);
+            TaskSolvingController controller = loader.getController();
+            controller.setCurrentTask(currentTaskSet.get(getSelectedItemIndex(taskSelector.getValue())), getSelectedItemIndex(taskSelector.getValue()), stage);
+            stage.show();
+            isTaskFinished[getSelectedItemIndex(taskSelector.getValue())] = true;
+        }
+    }
+
+    void updateSelector(int selectedItem)
+    {
+        ObservableList<String> items = FXCollections.observableArrayList();
+        for (int i = 0; i < currentTaskSet.size(); i++) items.add((i + 1) + ". " + currentTaskSet.get(i).getTaskName());
+        taskSelector.setItems(items);
+        taskSelector.setValue((selectedItem + 1) + ". " + currentTaskSet.get(selectedItem).getTaskName());
+    }
+
+    int getSelectedItemIndex(String item)
+    {
+        for (int i = 0; i < taskSelector.getItems().size(); i++)
+        {
+            if (taskSelector.getItems().get(i).equals(item)) return i;
+        }
+        return -1; //У випадку помилки
+    }
+
+    void showAlert(String text, Alert.AlertType type)
+    {
+        Alert error = new Alert(type); //Создание окна ошибки
+        error.setTitle("Information");
+        error.setContentText(text);
+        error.showAndWait();
+    }
+}
